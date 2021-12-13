53bff4d6e10784f6046780258c1346f0393a664e
Mike Akiba <mike2718@gmail.com>
2021-12-13 13:50:03 +0800

    添加许可

--- hhcx.c
+++ hhcx.c
@@ -5,18 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-void func(char *temp) {
+void func(char *temp)
+{
     char *name;
     name = strtok(temp, ":");
-    // 将位字符串转换为大写
+    // 将字符串转换为大写
     char *s = name;
-    while (*s) {
-        *s = toupper((unsigned char) *s);
+    while (*s)
+    {
+        *s = toupper((unsigned char) * s);
         s++;
     }
 }
-int main(void) {
-    const char *f[10][4] = {
+int main(void)
+{
+    const char *f[10][4] =
+    {
         {"北京"},
         {"黑龙江", "吉林", "辽宁"},
         {"天津", "内蒙古", "河北", "山西"},
@@ -31,13 +35,15 @@ int main(void) {
     char h[20];
     unsigned int i = 0;
     printf("\n\t业余无线电台呼号所属分区信息查询C程序\n\n");
-    printf("请输入要查询的呼号（例子：BH9YYY、bh1aaa）：");
-    if (fgets(h, 20, stdin)) {
+    printf("请输入要查询的呼号（例子：BH9YYY、bh1aa）：");
+    if (fgets(h, 20, stdin))
+    {
         h[strcspn(h, "\n")] = 0;
     }
     func(h);
     printf("\n该呼号信息：\n");
-    if (strlen(h) == 5 || strlen(h) == 6) { // 5位呼号、6位呼号
+    if (strlen(h) == 5 || strlen(h) == 6)   // 5位呼号、6位呼号
+    {
         if (h[0] != 'B')  // 国际电信联盟分配给中国
             printf("第一位字符%c并非中国的呼号！\n", h[0]);
         if (h[1] < 'A' || h[1] > 'Z')
@@ -51,17 +57,20 @@ int main(void) {
             printf("第二位字符%c：业余中继台\n", h[1]);
         if (h[2] < '0' || h[2] > '9')
             printf("第三位字符%c：电台分区号格式错误！\n", h[2]);
-        if (strlen(h) == 5 && h[3] && h[4] && !h[5] && (h[4] < 'A' || h[4] > 'Z')) // 最后一位不为字母
+        if (strlen(h) == 5 && h[3] && h[4] && !h[5] && (h[4] < 'A'
+                || h[4] > 'Z')) // 最后一位不为字母
             printf("最后一位字符%c不为字母！\n", h[4]);
         if (strlen(h) == 6 && h[3] && h[4] && h[5] && (h[5] < 'A' || h[5] > 'Z'))
             printf("最后一位字符%c不为字母！\n", h[5]);
-        switch (h[2]) {
+        switch (h[2])
+        {
         case '1':
             printf("第1区\t");
             if (h[3] >= 'A' && h[3] <= 'X')    // 北京
                 printf("%s\n", f[0][i]);
             // 试题格式YY/YYY对策
-            else if (strlen(h) == 5 && h[3] && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
+            else if (strlen(h) == 5 && h[3] && h[4] && !h[5] && (h[3] == 'Y'
+                     || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[3] && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c%c\n", h[3], h[4], h[5]);
@@ -71,9 +80,9 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'H')
                 printf("%s\n", f[1][i]);
             else if (h[3] >= 'I' && h[3] <= 'P')
-                printf("%s\n", f[1][i+1]);
+                printf("%s\n", f[1][i + 1]);
             else if (h[3] >= 'Q' && h[3] <= 'X')
-                printf("%s\n", f[1][i+2]);
+                printf("%s\n", f[1][i + 2]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -84,11 +93,11 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'F')
                 printf("%s\n", f[2][i]);
             else if (h[3] >= 'G' && h[3] <= 'L')
-                printf("%s\n", f[2][i+1]);
+                printf("%s\n", f[2][i + 1]);
             else if (h[3] >= 'M' && h[3] <= 'R')
-                printf("%s\n", f[2][i+2]);
+                printf("%s\n", f[2][i + 2]);
             else if (h[3] >= 'S' && h[3] <= 'X')
-                printf("%s\n", f[2][i+3]);
+                printf("%s\n", f[2][i + 3]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -99,9 +108,9 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'H')
                 printf("%s\n", f[3][i]);
             else if (h[3] >= 'I' && h[3] <= 'P')
-                printf("%s\n", f[3][i+1]);
+                printf("%s\n", f[3][i + 1]);
             else if (h[3] >= 'Q' && h[3] <= 'X')
-                printf("%s\n", f[3][i+2]);
+                printf("%s\n", f[3][i + 2]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -112,9 +121,9 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'H')
                 printf("%s\n", f[4][i]);
             else if (h[3] >= 'I' && h[3] <= 'P')
-                printf("%s\n", f[4][i+1]);
+                printf("%s\n", f[4][i + 1]);
             else if (h[3] >= 'Q' && h[3] <= 'X')
-                printf("%s\n", f[4][i+2]);
+                printf("%s\n", f[4][i + 2]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -125,9 +134,9 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'H')
                 printf("%s\n", f[5][i]);
             else if (h[3] >= 'I' && h[3] <= 'P')
-                printf("%s\n", f[5][i+1]);
+                printf("%s\n", f[5][i + 1]);
             else if (h[3] >= 'Q' && h[3] <= 'X')
-                printf("%s\n", f[5][i+2]);
+                printf("%s\n", f[5][i + 2]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -138,11 +147,11 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'H')
                 printf("%s\n", f[6][i]);
             else if (h[3] >= 'I' && h[3] <= 'P')
-                printf("%s\n", f[6][i+1]);
+                printf("%s\n", f[6][i + 1]);
             else if (h[3] >= 'Q' && h[3] <= 'X')
-                printf("%s\n", f[6][i+2]);
+                printf("%s\n", f[6][i + 2]);
             else if (h[3] >= 'Y' && h[3] <= 'Z')
-                printf("%s\n", f[6][i+3]);
+                printf("%s\n", f[6][i + 3]);
             //  AAA-ZZZ
             break;
         case '8':
@@ -150,11 +159,11 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'F')
                 printf("%s\n", f[7][i]);
             else if (h[3] >= 'G' && h[3] <= 'L')
-                printf("%s\n", f[7][i+1]);
+                printf("%s\n", f[7][i + 1]);
             else if (h[3] >= 'M' && h[3] <= 'R')
-                printf("%s\n", f[7][i+2]);
+                printf("%s\n", f[7][i + 2]);
             else if (h[3] >= 'S' && h[3] <= 'X')
-                printf("%s\n", f[7][i+3]);
+                printf("%s\n", f[7][i + 3]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -165,11 +174,11 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'F')
                 printf("%s\n", f[8][i]);
             else if (h[3] >= 'G' && h[3] <= 'L')
-                printf("%s\n", f[8][i+1]);
+                printf("%s\n", f[8][i + 1]);
             else if (h[3] >= 'M' && h[3] <= 'R')
-                printf("%s\n", f[8][i+2]);
+                printf("%s\n", f[8][i + 2]);
             else if (h[3] >= 'S' && h[3] <= 'X')
-                printf("%s\n", f[8][i+3]);
+                printf("%s\n", f[8][i + 3]);
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] == 'Y' || h[3] == 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
             else if (strlen(h) == 6 && h[4] && h[5] && (h[3] == 'Y' || h[3] == 'Z'))
@@ -181,7 +190,7 @@ int main(void) {
             if (h[3] >= 'A' && h[3] <= 'F')
                 printf("%s\n", f[9][i]);
             else if (h[3] >= 'G' && h[3] <= 'L')
-                printf("%s\n", f[9][i+1]);
+                printf("%s\n", f[9][i + 1]);
             // MAA-ZZZ
             else if (strlen(h) == 5 && h[4] && !h[5] && (h[3] >= 'M' || h[3] <= 'Z'))
                 printf("\n由国家无线电管理机构负责分配的呼号后缀：%c%c\n", h[3], h[4]);
@@ -190,46 +199,22 @@ int main(void) {
             break;
         }
     }
-    else if (strlen(h) == 4 || strlen(h) == 7) {  // 4位呼号、7位呼号，规则不明
+    else if (strlen(h) == 4)     // 4位呼号，规则不明
+    {
+        printf("由国家无线电管理机构负责分配的呼号：%c%c%c%c\n", h[0], h[1], h[2],
+               h[3]);
         if (h[0] != 'B')        // 国际电信联盟分配给中国
             printf("第一位字符%c并非中国的呼号！\n", h[0]);
-        else if (h[0] == 'B') {
-            switch (h[2]) {
-            case '1':
-                printf("第1区\n");
-                break;
-            case '2':
-                printf("第2区\n");
-                break;
-            case '3':
-                printf("第3区\n");
-                break;
-            case '4':
-                printf("第4区\n");
-                break;
-            case '5':
-                printf("第5区\n");
-                break;
-            case '6':
-                printf("第6区\n");
-                break;
-            case '7':
-                printf("第7区\t");
-                if (h[1] == 'S' && h[2] == '7' && h[3] == 'H') // BS7H
-                    printf("黄岩岛\n");
-                break;
-            case '8':
-                printf("第8区\n");
-                break;
-            case '9':
-                printf("第9区\n");
-                break;
-            case '0':
-                printf("第0区\n");
-                break;
-            }
-        }
-        printf("留作备用的呼号后缀：%c\n", h[3]);
+        else if (h[0] == 'B' && h[1] == 'S' && h[2] == '7' && h[3] == 'H') // BS7H
+            printf("第7区\t黄岩岛\n");
+    }
+    else if (strlen(h) == 7)     // 7位呼号，规则不明
+    {
+        printf("由国家无线电管理机构负责分配的呼号：%c%c%c%c%c%c%c\n", h[0], h[1],
+               h[2],
+               h[3], h[4], h[5], h[6]);
+        if (h[0] != 'B')        // 国际电信联盟分配给中国
+            printf("第一位字符%c并非中国的呼号！\n", h[0]);
     }
     else
         printf("\t呼号长度不正确！\n");
