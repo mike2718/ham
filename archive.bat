63456823c14509cdd1a3c922d4bd6d84cab70554
Mike Akiba <mike2718@gmail.com>
2021-12-26 16:16:58 +0800

    修订

--- archive.bat
+++ archive.bat
@@ -1,31 +1,18 @@
 @echo off
 cd /d "%~dp0"
-@rem 清理上次的打包文件
-@rem rm *.zip
-rm *.rar
-@rem 更新文件时间戳
-touch ham_a.pdf
-touch ham_b.pdf
-touch ham_c.pdf
-touch LICENSE.md
-touch README.md
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
+set datetime=%datetime:~0,4%.%datetime:~4,2%
 @rem 备份到上层目录
-cp ham_a.pdf ..
-cp ham_b.pdf ..
-cp ham_c.pdf ..
+cp -f ham_a.pdf ..
+cp -f ham_b.pdf ..
+cp -f ham_c.pdf ..
+rm ../ham.tar.xz
 git archive --format=tar.xz -o ../ham.tar.xz HEAD
-@rem 打包文件
-@rem D:\cmdtool\zip.exe -Z bzip2 ham_a.zip ham_a.pdf LICENSE.md README.md
-@rem D:\cmdtool\zip.exe -Z bzip2 ham_b.zip ham_b.pdf LICENSE.md README.md
-@rem D:\cmdtool\zip.exe -Z bzip2 ham_c.zip ham_c.pdf LICENSE.md README.md
-D:\cmdtool\rar.exe a -ma5 -m5 -ag_YYYY.MM ham_a.rar ham_a.pdf LICENSE.md README.md
-D:\cmdtool\rar.exe a -ma5 -m5 -ag_YYYY.MM ham_b.rar ham_a.pdf LICENSE.md README.md
-D:\cmdtool\rar.exe a -ma5 -m5 -ag_YYYY.MM ham_c.rar ham_a.pdf LICENSE.md README.md
-@rem 更新打包文件时间戳
-@rem touch ham_a.zip
-@rem touch ham_b.zip
-@rem touch ham_c.zip
-touch ham_a.rar
-touch ham_b.rar
-touch ham_c.rar
+@rem 打包
+touch ham_a.pdf ham_b.pdf ham_c.pdf LICENSE.md README.md
+rm *.zip
+D:\cmdtool\zip.exe -Z bzip2 -9 ham_a_%datetime%.zip ham_a.pdf LICENSE.md README.md
+D:\cmdtool\zip.exe -Z bzip2 -9 ham_b_%datetime%.zip ham_b.pdf LICENSE.md README.md
+D:\cmdtool\zip.exe -Z bzip2 -9 ham_c_%datetime%.zip ham_c.pdf LICENSE.md README.md
+touch *.zip
 pause
