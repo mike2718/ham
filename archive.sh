206b54e4f8be23ece41242017da2636ff428e04d
Mike Akiba <mike2718@gmail.com>
2022-01-25 03:57:47 +0800

    修正

--- archive.sh
+++ archive.sh
@@ -3,11 +3,9 @@ cp -f ham_a.pdf ..
 cp -f ham_b.pdf ..
 cp -f ham_c.pdf ..
 rm -f ham_*.zip
-touch ham_a.pdf ham_b.pdf ham_c.pdf LICENSE.md README.md
 zip "ham_a_"`date +"%Y.%m.zip"` ham_a.pdf LICENSE.md README.md
 zip "ham_b_"`date +"%Y.%m.zip"` ham_b.pdf LICENSE.md README.md
 zip "ham_c_"`date +"%Y.%m.zip"` ham_c.pdf LICENSE.md README.md
-touch ham_*.zip
 unzip -l "ham_a_"`date +"%Y.%m.zip"`
 unzip -l "ham_b_"`date +"%Y.%m.zip"`
 unzip -l "ham_c_"`date +"%Y.%m.zip"`
