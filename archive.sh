b9aeed20baa834d750e388a971ce2ff67ae5ceb8
Mike Akiba <mike2718@gmail.com>
2022-02-02 18:00:11 +0800

    修改打包脚本

--- archive.sh
+++ archive.sh
@@ -2,11 +2,10 @@
 cp -f ham_a.pdf ..
 cp -f ham_b.pdf ..
 cp -f ham_c.pdf ..
-rm -f ham_*.zip
-zip "ham_a_"`date +"%Y.%m.zip"` ham_a.pdf LICENSE.md README.md
-zip "ham_b_"`date +"%Y.%m.zip"` ham_b.pdf LICENSE.md README.md
-zip "ham_c_"`date +"%Y.%m.zip"` ham_c.pdf LICENSE.md README.md
-unzip -l "ham_a_"`date +"%Y.%m.zip"`
-unzip -l "ham_b_"`date +"%Y.%m.zip"`
-unzip -l "ham_c_"`date +"%Y.%m.zip"`
-
+rm -f ham_*.7z
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_a_"`date +"%Y.%m.7z"` ham_a.pdf LICENSE.md README.md
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_b_"`date +"%Y.%m.7z"` ham_b.pdf LICENSE.md README.md
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_c_"`date +"%Y.%m.7z"` ham_c.pdf LICENSE.md README.md
+7za l "ham_a_"`date +"%Y.%m.7z"`
+7za l "ham_b_"`date +"%Y.%m.7z"`
+7za l "ham_c_"`date +"%Y.%m.7z"`
