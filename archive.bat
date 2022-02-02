62bc564eced6a4e876bd8e02195e9b2f0e9879d3
Mike Akiba <mike2718@gmail.com>
2022-02-02 18:00:11 +0800

    修改打包脚本

--- archive.bat
+++ archive.bat
@@ -2,17 +2,16 @@
 cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%.%datetime:~4,2%
-@rem 备份到上层目录
 xcopy ham_a.pdf .. /y /c
 xcopy ham_b.pdf .. /y /c
 xcopy ham_c.pdf .. /y /c
 del /q ..\ham.tar.zst
-git archive --format=tar.zst -o ../ham.tar.zst HEAD
-@rem 打包
-touch ham_a.pdf ham_b.pdf ham_c.pdf LICENSE.md README.md
-del /q ham_a_*.zip ham_b_*.zip ham_c_*.zip
-D:\cmdtool\zip.exe -Z bzip2 ham_a_%datetime%.zip ham_a.pdf LICENSE.md README.md
-D:\cmdtool\zip.exe -Z bzip2 ham_b_%datetime%.zip ham_b.pdf LICENSE.md README.md
-D:\cmdtool\zip.exe -Z bzip2 ham_c_%datetime%.zip ham_c.pdf LICENSE.md README.md
-touch ham_a_%datetime%.zip ham_b_%datetime%.zip ham_c_%datetime%.zip
+git archive --format=tar.zst --prefix=ham/ --output=../ham.tar.zst HEAD
+del /q ham_*.7z
+D:\cmdtool\7za.exe a -t7z -mx9 ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
+D:\cmdtool\7za.exe a -t7z -mx9 ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
+D:\cmdtool\7za.exe a -t7z -mx9 ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
+D:\cmdtool\7za.exe l ham_a_%datetime%.7z
+D:\cmdtool\7za.exe l ham_b_%datetime%.7z
+D:\cmdtool\7za.exe l ham_c_%datetime%.7z
 pause
