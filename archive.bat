e768c6dc2919688c71ed05fb6fd2a30370b15c0f
Mike Akiba <mike2718@gmail.com>
2022-02-05 13:37:36 +0800

    更新脚本

--- archive.bat
+++ archive.bat
@@ -2,16 +2,18 @@
 cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%.%datetime:~4,2%
-xcopy ham_a.pdf .. /y /c
-xcopy ham_b.pdf .. /y /c
-xcopy ham_c.pdf .. /y /c
-del /q ..\ham.tar.zst
-git archive --format=tar.zst --prefix=ham/ --output=../ham.tar.zst HEAD
-del /q ham_*.7z
-D:\cmdtool\7za.exe a -t7z -mx9 ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
-D:\cmdtool\7za.exe a -t7z -mx9 ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
-D:\cmdtool\7za.exe a -t7z -mx9 ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
-D:\cmdtool\7za.exe l ham_a_%datetime%.7z
-D:\cmdtool\7za.exe l ham_b_%datetime%.7z
-D:\cmdtool\7za.exe l ham_c_%datetime%.7z
+del /q ..\ham_*.pdf 2>nul
+copy /y ham_a.pdf ..\ham_a.pdf >nul
+copy /y ham_b.pdf ..\ham_a.pdf >nul
+copy /y ham_c.pdf ..\ham_a.pdf >nul
+rem compress 7zip
+del /q ham_*.7z 2>nul
+D:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
+D:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
+D:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
+rem compress zstd
+del /q ham_*.tar.zst 2>nul
+C:\msys64\usr\bin\tar.exe --zstd -cvf ham_a_%datetime%.tar.zst ham_a.pdf LICENSE.md README.md
+C:\msys64\usr\bin\tar.exe --zstd -cvf ham_b_%datetime%.tar.zst ham_b.pdf LICENSE.md README.md
+C:\msys64\usr\bin\tar.exe --zstd -cvf ham_c_%datetime%.tar.zst ham_c.pdf LICENSE.md README.md
 pause
