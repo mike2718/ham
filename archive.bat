c8ed92a731b035b003e4cb3d68ae4844a45b06d8
Mike Akiba <mike2718@gmail.com>
2022-03-28 02:09:16 +0800

    更新打包批处理

--- archive.bat
+++ archive.bat
@@ -8,25 +8,15 @@ COPY /Y ham_a.pdf ..\ >nul
 COPY /Y ham_b.pdf ..\ >nul
 COPY /Y ham_c.pdf ..\ >nul
 rem get hash
-git rev-parse HEAD > git-hash.txt
-rem compress zip
-del /q ham_*.zip 2>nul
-D:\cmdtool\zip.exe ham_a_%datetime%.zip ham_a.pdf LICENSE.md README.md
-D:\cmdtool\zip.exe -z ham_a_%datetime%.zip < git-hash.txt
-D:\cmdtool\zip.exe ham_b_%datetime%.zip ham_b.pdf LICENSE.md README.md
-D:\cmdtool\zip.exe -z ham_b_%datetime%.zip < git-hash.txt
-D:\cmdtool\zip.exe ham_c_%datetime%.zip ham_c.pdf LICENSE.md README.md
-D:\cmdtool\zip.exe -z ham_c_%datetime%.zip < git-hash.txt
-rem compress 7z
-del /q ham_*.7z 2>nul
-D:\cmdtool\7z.exe a -t7z ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
-D:\cmdtool\7z.exe a -t7z ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
-D:\cmdtool\7z.exe a -t7z ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
+rem git rev-parse HEAD > git-hash.txt
+del /q ham_*.tar.zst 2>nul
+D:\cmdtool\7z.exe -ttar a ham_a ham_a.pdf LICENSE.md README.md -so | zstd  -T0 --ultra -20 -o ham_a_%datetime%.tar.zst
+D:\cmdtool\7z.exe -ttar a ham_b ham_b.pdf LICENSE.md README.md -so | zstd  -T0 --ultra -20 -o ham_b_%datetime%.tar.zst
+D:\cmdtool\7z.exe -ttar a ham_c ham_c.pdf LICENSE.md README.md -so | zstd  -T0 --ultra -20 -o ham_c_%datetime%.tar.zst
 rem generate sha256sums
-rem D:\cmdtool\generic_sum.exe SHA256 ham_*.zip > sha256sum.txt
-D:\cmdtool\generic_sum.exe SHA256 ham_a_*.zip > sha256sum.txt
-D:\cmdtool\generic_sum.exe SHA256 ham_b_*.zip >> sha256sum.txt
-COPY /Y ham_a_*.zip ..\
-COPY /Y ham_b_*.zip ..\
+D:\cmdtool\generic_sum.exe SHA256 ham_a_*.tar.zst > sha256sum.txt
+D:\cmdtool\generic_sum.exe SHA256 ham_b_*.tar.zst >> sha256sum.txt
+COPY /Y ham_a_*.tar.zst ..\
+COPY /Y ham_b_*.tar.zst ..\
 D:\cmdtool\awk.exe "{ print \"- \" $2 \" µÄ SHA256 ¹þÏ£:\"; print \"`\" $1 \"`\" }" sha256sum.txt > ..\sha256sums.txt
 pause
