607a6886085cbb6909a738229d45392d24786fe4
Mike Akiba <mike2718@gmail.com>
2022-07-10 01:14:28 +0800

    修改批处理

--- archive.cmd
+++ archive.cmd
@@ -5,12 +5,12 @@ rem set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
 set datetime=%datetime:~0,4%.%datetime:~4,2%
 rem git archive -v --format=tar.zst --prefix=ham_%datetime%_HEAD/ -19 --output=../ham_%datetime%_HEAD.tar.zst HEAD
 del /q ..\ham_*.7z ham_*.7z 2>nul
-COPY /Y ham_a.pdf .. >nul
-COPY /Y ham_b.pdf .. >nul
-COPY /Y ham_c.pdf .. >nul
-E:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
-E:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
-E:\cmdtool\7z.exe a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
+copy /y ham_a.pdf .. >nul
+copy /y ham_b.pdf .. >nul
+copy /y ham_c.pdf .. >nul
+E:\cmdtool\7z.exe a -t7z ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
+E:\cmdtool\7z.exe a -t7z ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
+E:\cmdtool\7z.exe a -t7z ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
 rem generate sha256sums
-E:\cmdtool\generic_sum.exe SHA256 ham_a_*.7z ham_b_*.7z > ..\sha256sum.txt
+E:\cmdtool\generic_sum.exe SHA256 ham_*.7z > sha256sums.txt
 pause
