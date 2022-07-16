f581e23a8927c93cb5316000b736534769ff9027
Mike Akiba <mike2718@gmail.com>
2022-07-16 20:15:39 +0800

    修订、更新打包脚本

--- archive.sh
+++ archive.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
-cp -f ham_a.pdf ..
-cp -f ham_b.pdf ..
-cp -f ham_c.pdf ..
 rm -f ham_*.7z
-7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_a_"`date +"%Y.%m.7z"` ham_a.pdf LICENSE.md README.md
-7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_b_"`date +"%Y.%m.7z"` ham_b.pdf LICENSE.md README.md
-7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "ham_c_"`date +"%Y.%m.7z"` ham_c.pdf LICENSE.md README.md
+7z a -t7z "ham_a_"`date +"%Y.%m.7z"` ham_a.pdf README.md
+7z a -t7z "ham_b_"`date +"%Y.%m.7z"` ham_b.pdf README.md
+7z a -t7z "ham_c_"`date +"%Y.%m.7z"` ham_c.pdf README.md
 7z l "ham_a_"`date +"%Y.%m.7z"`
 7z l "ham_b_"`date +"%Y.%m.7z"`
 7z l "ham_c_"`date +"%Y.%m.7z"`
+
