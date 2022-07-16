5c1f42cdecabceaf8b42ce82c0273f31d115ae92
Mike Akiba <mike2718@gmail.com>
2022-07-16 20:15:39 +0800

    修订、更新打包脚本

--- archive.cmd
+++ archive.cmd
@@ -8,9 +8,9 @@ del /q ..\ham_*.7z ham_*.7z 2>nul
 copy /y ham_a.pdf .. >nul
 copy /y ham_b.pdf .. >nul
 copy /y ham_c.pdf .. >nul
-E:\cmdtool\7z.exe a -t7z ham_a_%datetime%.7z ham_a.pdf LICENSE.md README.md
-E:\cmdtool\7z.exe a -t7z ham_b_%datetime%.7z ham_b.pdf LICENSE.md README.md
-E:\cmdtool\7z.exe a -t7z ham_c_%datetime%.7z ham_c.pdf LICENSE.md README.md
+E:\cmdtool\7z.exe a -t7z ham_a_%datetime%.7z ham_a.pdf README.md
+E:\cmdtool\7z.exe a -t7z ham_b_%datetime%.7z ham_b.pdf README.md
+E:\cmdtool\7z.exe a -t7z ham_c_%datetime%.7z ham_c.pdf README.md
 rem generate sha256sums
 E:\cmdtool\generic_sum.exe SHA256 ham_*.7z > sha256sums.txt
 pause
