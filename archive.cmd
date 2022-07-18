fef14286ff0fa52080d2b81176f1a1536a216a15
Mike Akiba <mike2718@gmail.com>
2022-07-18 20:02:16 +0800

    更新打包脚本

--- archive.cmd
+++ archive.cmd
@@ -1,16 +1,14 @@
 @echo off
 cd /d "%~dp0"
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
-rem set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
-set datetime=%datetime:~0,4%.%datetime:~4,2%
-rem git archive -v --format=tar.zst --prefix=ham_%datetime%_HEAD/ -19 --output=../ham_%datetime%_HEAD.tar.zst HEAD
-del /q ..\ham_*.7z ham_*.7z 2>nul
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%%MyDate:~4,2%%MyDate:~6,2%
+rem git archive -v --format=tar.zst --prefix=ham_%today%_HEAD/ -19 --output=../ham_%today%_HEAD.tar.zst HEAD
+del /q ham_*.7z 2>nul
 copy /y ham_a.pdf .. >nul
 copy /y ham_b.pdf .. >nul
 copy /y ham_c.pdf .. >nul
-E:\cmdtool\7z.exe a -t7z ham_a_%datetime%.7z ham_a.pdf README.md
-E:\cmdtool\7z.exe a -t7z ham_b_%datetime%.7z ham_b.pdf README.md
-E:\cmdtool\7z.exe a -t7z ham_c_%datetime%.7z ham_c.pdf README.md
-rem generate sha256sums
-E:\cmdtool\generic_sum.exe SHA256 ham_*.7z > sha256sums.txt
+E:\cmdtool\7z.exe a -t7z ham_a_%today%.7z ham_a.pdf README.md
+E:\cmdtool\7z.exe a -t7z ham_b_%today%.7z ham_b.pdf README.md
+E:\cmdtool\7z.exe a -t7z ham_c_%today%.7z ham_c.pdf README.md
+E:\cmdtool\generic_sum.exe SHA1 ham_*.7z > sha1sums.txt
 pause
