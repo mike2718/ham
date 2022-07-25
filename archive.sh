855dc64efb6463432acba6731ffdfcd8a687f4ca
Mike Akiba <mike2718@gmail.com>
2022-07-25 19:40:26 +0800

    改文件名

--- archive.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-rm -f ham_*.7z
-7z a -t7z "ham_a_"`date +"%Y.%m.7z"` ham_a.pdf README.md
-7z a -t7z "ham_b_"`date +"%Y.%m.7z"` ham_b.pdf README.md
-7z a -t7z "ham_c_"`date +"%Y.%m.7z"` ham_c.pdf README.md
-7z l "ham_a_"`date +"%Y.%m.7z"`
-7z l "ham_b_"`date +"%Y.%m.7z"`
-7z l "ham_c_"`date +"%Y.%m.7z"`
-
---
+++ build.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+rem 用astyle美观化
+
+rem 编译pdf文件
+pause
