11250c2d5e542902061ced40c242d9a0133f9a6b
Mike Akiba <mike2718@gmail.com>
2023-11-17 01:25:59 +0800

    调整缩进和换行

--- make.cmd
+++ make.cmd
@@ -7,7 +7,8 @@ set datetime=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%
 del /q ..\ham_*.zip ..\SHA256SUMS.txt 2>nul
 del /q *.obj *.exe 2>nul
 del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc 2>nul
-rd /s /q out
+del /q *.bak0 *.bak1 2>nul
+rd /s /q out 2>nul
 
 C:\cmdtool32\astyle.exe -A1 -p -s4 -xC80 -c hhcx_a.c icao_a.c jisuan_a.c
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 hhcx_a.c > hhcx.c
@@ -17,6 +18,18 @@ C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 jisuan_a.c > jisuan.c
 rem C:\mingw64\bin\x86_64-w64-mingw32-gcc.exe -Wall -Wpedantic -Wextra -std=c99 -o hhcx_a.exe hhcx_a.c
 rem C:\mingw64\bin\x86_64-w64-mingw32-gcc.exe -Wall -Wpedantic -Wextra -std=c99 -o icao_a.exe icao_a.c
 rem C:\mingw64\bin\x86_64-w64-mingw32-gcc.exe -Wall -Wpedantic -Wextra -std=c99 -o jisuan_a.exe jisuan_a.c
+
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml ham_a.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml ham_b.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml ham_c.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml fulu_a.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml fulu_b.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml fulu_c.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml bianzhuzhedehua.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml cankaowenxian.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml houji.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml qianyan.tex
+C:\cmdtool64\latexindent.exe -w -m -s -l latexindent.yaml xuke.tex
 
 xelatex.exe ham_a.tex
 bibtex.exe ham_a
