182c50b1cc792823cca622d5158b12de7bd4ccda
Mike Akiba <mike2718@gmail.com>
2023-11-23 00:44:41 +0800

    重写将呼号转换成字母解释法的单词组合的C程序

--- icao.c
+++ icao.c
@@ -1,173 +1,113 @@
 // 将呼号转换成字母解释法的单词组合的C程序
 // 用法：
-//   $ pcc -Wall -Wpedantic -Wextra -std=c99 -o icao icao.c
+//   $ gcc -Wall -Wpedantic -Wextra -std=c99 -o icao icao.c
 //   $ ./icao
+// 特别鸣谢：ChatGPT
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
+
+const char* natoTranslate(char c)
+{
+    switch (toupper(c))
+    {
+    case 'A':
+        return "Alfa";
+    case 'B':
+        return "Bravo";
+    case 'C':
+        return "Charlie";
+    case 'D':
+        return "Delta";
+    case 'E':
+        return "Echo";
+    case 'F':
+        return "Foxtrot";
+    case 'G':
+        return "Golf";
+    case 'H':
+        return "Hotel";
+    case 'I':
+        return "India";
+    case 'J':
+        return "Juliett";
+    case 'K':
+        return "Kilo";
+    case 'L':
+        return "Lima";
+    case 'M':
+        return "Mike";
+    case 'N':
+        return "November";
+    case 'O':
+        return "Oscar";
+    case 'P':
+        return "Papa";
+    case 'Q':
+        return "Quebec";
+    case 'R':
+        return "Romeo";
+    case 'S':
+        return "Sierra";
+    case 'T':
+        return "Tango";
+    case 'U':
+        return "Uniform";
+    case 'V':
+        return "Victor";
+    case 'W':
+        return "Whiskey";
+    case 'X':
+        return "X-ray";
+    case 'Y':
+        return "Yankee";
+    case '0':
+        return "Zero";
+    case '1':
+        return "One";
+    case '2':
+        return "Two";
+    case '3':
+        return "Three";
+    case '4':
+        return "Four";
+    case '5':
+        return "Five";
+    case '6':
+        return "Six";
+    case '7':
+        return "Seven";
+    case '8':
+        return "Eight";
+    case '9':
+        return "Nine";
+    case '/':
+        return "Stroke";
+    default:
+        return 0;
+    }
+}
+
 int main (void)
 {
-    char word[10] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
-    char phonetic[100] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0',
-                          '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'
-                         };
-    unsigned int count = 0;
-    printf("\n\t将呼号转换成字母解释法的单词组合的C程序\n\n");
-    printf("请输入要转换的呼号：");
-    if (fgets(word, 10, stdin))
+    printf("请输入要翻译的呼号：");
+    char ch[64];
+    int i;
+    if (fgets(ch, 64, stdin))
     {
-        word[strcspn(word, "\n")] = 0;
+        ch[strcspn(ch, "\n")] = 0;
     }
-    while(count < sizeof(word))
+    for (i = 0; ch[i] != '\0'; i++)
     {
-        switch (word[count])
-        {
-        case 'A':
-        case 'a':
-            strcat(phonetic, " Alfa");
-            break;
-        case 'B':
-        case 'b':
-            strcat(phonetic, " Bravo");
-            break;
-        case 'C':
-        case 'c':
-            strcat(phonetic, " Charlie");
-            break;
-        case 'D':
-        case 'd':
-            strcat(phonetic, " Delta");
-            break;
-        case 'E':
-        case 'e':
-            strcat(phonetic, " Echo");
-            break;
-        case 'F':
-        case 'f':
-            strcat(phonetic, " Foxtrot");
-            break;
-        case 'G':
-        case 'g':
-            strcat(phonetic, " Golf");
-            break;
-        case 'H':
-        case 'h':
-            strcat(phonetic, " Hotel");
-            break;
-        case 'I':
-        case 'i':
-            strcat(phonetic, " India");
-            break;
-        case 'J':
-        case 'j':
-            strcat(phonetic, " Juliett");
-            break;
-        case 'K':
-        case 'k':
-            strcat(phonetic, " Kilo");
-            break;
-        case 'L':
-        case 'l':
-            strcat(phonetic, " Lima");
-            break;
-        case 'M':
-        case 'm':
-            strcat(phonetic, " Mike");
-            break;
-        case 'N':
-        case 'n':
-            strcat(phonetic, " November");
-            break;
-        case 'O':
-        case 'o':
-            strcat(phonetic, " Oscar");
-            break;
-        case 'P':
-        case 'p':
-            strcat(phonetic, " Papa");
-            break;
-        case 'Q':
-        case 'q':
-            strcat(phonetic, " Quebec");
-            break;
-        case 'R':
-        case 'r':
-            strcat(phonetic, " Romeo");
-            break;
-        case 'S':
-        case 's':
-            strcat(phonetic, " Sierra");
-            break;
-        case 'T':
-        case 't':
-            strcat(phonetic, " Tango");
-            break;
-        case 'U':
-        case 'u':
-            strcat(phonetic, " Uniform");
-            break;
-        case 'V':
-        case 'v':
-            strcat(phonetic, " Victor");
-            break;
-        case 'W':
-        case 'w':
-            strcat(phonetic, " Whiskey");
-            break;
-        case 'X':
-        case 'x':
-            strcat(phonetic, " X-ray");
-            break;
-        case 'Y':
-        case 'y':
-            strcat(phonetic, " Yankee");
-            break;
-        case 'Z':
-        case 'z':
-            strcat(phonetic, " Zulu");
-            break;
-        case '0':
-            strcat(phonetic, " Zero");
-            break;
-        case '1':
-            strcat(phonetic, " One");
-            break;
-        case '2':
-            strcat(phonetic, " Two");
-            break;
-        case '3':
-            strcat(phonetic, " Three");
-            break;
-        case '4':
-            strcat(phonetic, " Four");
-            break;
-        case '5':
-            strcat(phonetic, " Five");
-            break;
-        case '6':
-            strcat(phonetic, " Six");
-            break;
-        case '7':
-            strcat(phonetic, " Seven");
-            break;
-        case '8':
-            strcat(phonetic, " Eight");
-            break;
-        case '9':
-            strcat(phonetic, " Nine");
-            break;
-            //default: printf("您输入的部分内容非字母和数字。\n");
-        }
-        count++;
+        if (isalnum(ch[i]) || ch[i] == '/')
+            printf("%c", ch[i]);
     }
-    printf("呼号%s的字母解释法是：%s\n", word, phonetic);
+    printf("的字母解释法是：");
+    for (i = 0; ch[i] != '\0'; i++)
+    {
+        if (isalnum(ch[i]) || ch[i] == '/')
+            printf("%s ", natoTranslate(ch[i]));
+    }
     printf("\n");
     return 0;
 }
